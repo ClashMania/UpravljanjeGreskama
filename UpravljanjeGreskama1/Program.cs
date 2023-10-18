07342c0ebe9fde5c3563a1d606e72bea31c5c049
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-10-18 08:24:13 +0200

    Add files via upload

---
+++ UpravljanjeGreskama1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpravljanjeGreskama1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola=false;
+
+
+
+          
+
+            do
+            {  
+                Console.WriteLine("Upišite svoju godinu rođenja: ");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Rođen si " + godRod + ". godine =D");
+                }
+
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                }
+
+            }
+            while (kontrola == false);
+           
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
